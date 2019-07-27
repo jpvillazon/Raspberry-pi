0fab072abd00cd844b8e1a3e0ffbfac864f1bcd7
Juan Pablo Villazon (JuanPi) <37195739+jpvillazon@users.noreply.github.com>
2019-07-26 22:39:50 -0400

    Update stream_firebase_realtime_db.py
    adding quitting conditional

--- firebase/python/stream_firebase_realtime_db.py
+++ firebase/python/stream_firebase_realtime_db.py
@@ -14,12 +14,14 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 def stream_handler(message):
+    if message["data"]== True and message["path"] == "/quit":
+      print "Quitting"
+      quit()
+      
     print(message["event"])
     print(message["path"])
     print(message["data"])
+    
 
-try:
-    my_stream = db.child(“database”).stream(stream_handler)
-except KeyboardInterrupt:
-    my_stream.close()
-    sys.exit()
+my_stream = db.child("database").stream(stream_handler)
+#my_stream.close()
